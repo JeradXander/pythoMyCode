75d877717fd09a5c95793c58161b699c1a51d1f8
Jerad Alexander <jeradalex@gmail.com>
2021-06-07 21:00:19 +0000

    Committing lab 10

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,7 @@
+#! usr/bin/env python3
+
+user_input = input("Please enther an Ipv4  IP Address:")
+
+print("You told me the IPv4 address is:" + user_input)
+
+
---
+++ listlabs/workingWithLists.py
@@ -0,0 +1,6 @@
+partyList = ["snacks", "uncle who drinks to muck","beer","grill", "bangers", "live entertainment", "silverware",]
+
+print(f"I'll have my own party! With {partyList[0]} and {partyList[5]}")
+
+partyList2 = [["snacks","beer","bangers"], ["uncle that drinks too much//live entertainment"], ["grill","silverware"]]
+print(f"I will bring the {partyList2[-1][-1]}!")
