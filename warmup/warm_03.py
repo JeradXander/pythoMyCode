af3298c5156d8e7cf80a42a1c316d4b2c661c4dc
Jerad Alexander <jeradal@amazon.com>
2021-06-15 08:51:47 -0700

    pushing marvel api

--- warmup/warm_03.py
+++ warmup/warm_03.py
@@ -4,18 +4,13 @@
 import requests
 
 APIURI = "http://api.open-notify.org/astros.json"
-
 astroList = [];
 
 def main():
     
     apirequest = requests.get(APIURI)
-    
     reqToJSON = apirequest.json()
-    
-    
     numberOfAstros = reqToJSON["number"]
-    
     print(f"There are {numberOfAstros} in space currently")
     
     for astros in reqToJSON["people"]:
@@ -26,9 +21,7 @@ def main():
         
         print(f"{name} is currently in space on the {craft}")
         
-
 if __name__ == "__main__":
     main()
-    
     print(astroList)
         
